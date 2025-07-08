0f58a2f062eff7f1228da4e50f84b3562fb984f8
Sebastián Damis <sebastian.damis@gmail.com>
2025-07-08 03:11:12 -0300

    4th

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+interface Contacto {
+  nombre: string,
+  apellido: string,
+  email: string,
+  asunto: string,
+  comentario: string
+}
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class Contact {
 
+
+export class Contact {
+  contact : Contacto[] = []
+
+  submitForm(){
+    console.log(this.formState)
+    this.contact.push({
+      nombre: this.formState.nombre,
+      apellido: this.formState.apellido,
+      email: this.formState.email,
+      asunto: this.formState.asunto,
+      comentario: this.formState.comentario
+    })
+    this.formState.nombre = "",
+    this.formState.apellido = "",
+    this.formState.email = "",
+    this.formState.asunto = "",
+    this.formState.comentario = ""
+  }
+  formState = {
+    nombre: "",
+    apellido: "",
+    email: "",
+    asunto: "",
+    comentario: ""
+  }
 }
